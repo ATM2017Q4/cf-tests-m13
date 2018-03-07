32809f7733809b6ea5e7ee631761441678203512
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-03-07 20:28:20 +0300

    Add LoggerUtil class

--- src/test/java/com/cheapflights/ui/utils/LoggerUtil.java
+++ src/test/java/com/cheapflights/ui/utils/LoggerUtil.java
@@ -5,5 +5,31 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 public class LoggerUtil {
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static Logger logger = LogManager.getLogger();
+
+    public static void error(String msg) {
+        logger.error(msg);
+    }
+
+    public static void error(String msg, Throwable t) {
+        logger.error(msg, t);
+    }
+
+    public static void info(String msg) {
+        logger.info(msg);
+    }
+
+    public static void debug(String msg) {
+        logger.debug(msg);
+    }
+
+    public static void warn(String msg) {
+        logger.warn(msg);
+    }
+
+    public static void trace(String msg) {
+        logger.trace(msg);
+    }
+    
+
 }
