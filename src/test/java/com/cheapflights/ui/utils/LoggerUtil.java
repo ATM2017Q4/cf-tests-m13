c0dc1468cb3dade3a45042ae20d55395527affd5
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-03-07 08:25:15 +0300

    Add pom.xml dependencies, create LoggerUtil class

---
+++ src/test/java/com/cheapflights/ui/utils/LoggerUtil.java
@@ -0,0 +1,9 @@
+package com.cheapflights.ui.utils;
+
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class LoggerUtil {
+    private static final Logger LOGGER = LogManager.getLogger();
+}
